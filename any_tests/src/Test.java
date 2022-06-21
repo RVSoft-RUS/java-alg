e1ca093caf2055de079b89faee7f4d1225d083d9
Смирнов Роман Валентинович [ДЗО] <RValeSmirnov@sberbank.ru>
2022-06-21 15:04:13 +0300

    12.06.22

--- any_tests/src/Test.java
+++ any_tests/src/Test.java
@@ -1,5 +1,10 @@
 public class Test {
     public static void main(String[] args) {
-
+        Object[] objects =new Object[5];
+        objects[0] = "the String";
+        objects[3] = "the String";
+        String s = (String) objects[0];
+        String s1 = (String) objects[1];
+        System.out.println(s + ", " + s1);
     }
 }
