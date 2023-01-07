a78b7fa866ee2f03994bc6cb35d1b0c4bef9d289
20167295 <RValenSmirnov@sberbank.ru>
2023-01-07 19:06:32 +0300

    + visitor, templatemethod

---
+++ patterns/behavioral/templatemethod/Example.java
@@ -0,0 +1,64 @@
+package templatemethod;
+
+public class Example {
+    public static void main(String[] args) {
+        HouseTemplate houseTemplate = new WoodenHouse();
+
+        houseTemplate.buildHouse();
+        System.out.println("********************");
+
+        houseTemplate = new GlassHouse();
+        houseTemplate.buildHouse();
+    }
+}
+
+abstract class HouseTemplate {
+
+    //template method, final so subclasses can't override
+    public final void buildHouse(){
+        buildFoundation();
+        buildPillars();
+        buildWalls();
+        buildWindows();
+        System.out.println("House is built.");
+    }
+
+    //default implementation
+    private void buildWindows() {
+        System.out.println("Building Glass Windows");
+    }
+
+    //methods to be implemented by subclasses
+    public abstract void buildWalls();
+    public abstract void buildPillars();
+
+    private void buildFoundation() {
+        System.out.println("Building foundation with cement,iron rods and sand");
+    }
+}
+
+class WoodenHouse extends HouseTemplate {
+
+    @Override
+    public void buildWalls() {
+        System.out.println("Building Wooden Walls");
+    }
+
+    @Override
+    public void buildPillars() {
+        System.out.println("Building Pillars with Wood coating");
+    }
+}
+
+class GlassHouse extends HouseTemplate {
+
+    @Override
+    public void buildWalls() {
+        System.out.println("Building Glass Walls");
+    }
+
+    @Override
+    public void buildPillars() {
+        System.out.println("Building Pillars with glass coating");
+    }
+}
