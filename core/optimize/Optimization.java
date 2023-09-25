1a1f274848297471ba5df04b58035fa2ebfb9da1
vtb4091497 <rsmirnov@dev.vtb.ru>
2023-09-25 23:33:25 +0300

    remote debug

---
+++ core/optimize/Optimization.java
@@ -0,0 +1,10 @@
+package optimize;
+
+//import org.apache.commons.lang3.StringUtils;
+
+public class Optimization {
+    public static void main(String[] args) {
+        //Вместо использующую регулярку replaceAll
+//        str = StringUtils.replace(str, "«", "\"");
+    }
+}
